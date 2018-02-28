b7d6b31a1541c512ee4b31e893fe8d7e95aa9ed8
Ilya Radu <raduilya@gmail.com>
2018-02-28 15:42:00 +0200

    add new test correct input in telegram

--- ApiBot/TelegramApi.js
+++ ApiBot/TelegramApi.js
@@ -1,5 +1,10 @@
 const TelegramBot = require('node-telegram-bot-api');
 const configApi = require('./conf.json');
+let mongo = require('mongodb');
+let MongoClient = mongo.MongoClient;
+
+const DATABASE_URL = 'mongodb://localhost:27017/test12';
+
 class TelegramApi{
   constructor(){
     this.api = null;
@@ -14,5 +19,44 @@ class TelegramApi{
     return true;
   }
 
+  getTelegramControllers() {
+    return {
+      'add_category': {
+        'function': this.addCategory,
+        'regex': /\/add_category (.+)/
+    }}
+  }
+
+  addCategory(message, matched_list) {
+    const {from: {id}} = message;
+    const response = matched_list[1]; // the captured "whatever"
+    let categories = response.split(' ');
+        console.log('add categories: ', categories[0].toLowerCase());
+    if (categories.length === 1 && !message.from.is_bot) {
+        let foundAccount = null;
+        MongoClient.connect(DATABASE_URL, function (err, db) {
+        if (err) throw err;
+        console.log("id", id);
+        try {
+          foundAccount = db.collection("categories").insertOne({category: categories[0].toLowerCase()});
+          db.close();
+          this.api.sendMessage(id, 'good! we added ' + categories[0]);
+        } catch (e) {
+          console.log(e);
+          this.api.sendMessage(id, 'wrong name, please resent the categories! in one string');
+        }
+
+      });
+    }
+    else {
+      this.api.sendMessage(id, 'wrong name, please resent the categories! in one string');
+    }
+  }
+
+  // addControllers(controllers) {
+  //   controllers.forEach(controller => {
+  //     this.api.onText(controller)
+  //   })
+  // }
 }
 module.exports = TelegramApi;
--- test/TestTelegramApi.js
+++ test/TestTelegramApi.js
@@ -1,5 +1,6 @@
 import 'babel-polyfill';
 import TelegramApi from '../ApiBot/TelegramApi.js';
+import chai from 'chai';
 import assert from 'assert';
 
 let telegramApi = new TelegramApi();
@@ -9,9 +10,16 @@ describe('TestTelegramApi() Create Telegram interface chat', function() {
      telegramApi = new TelegramApi();
   });
 
-  it('what am i testing..', function() {
+  it('init method in TelegramApi.init() ', function() {
     let didIitialSuccessfully = telegramApi.init();
+    assert.equal(telegramApi.api !== null, true);
     assert.equal(didIitialSuccessfully, true);
+  });
+  it('add functionality to TelegramApi', function() {
+    telegramApi.init();
+    let controllers = telegramApi.getTelegramControllers();
+    assert.ok(controllers['add_category'] !== undefined);
+    // telegramApi.addControllers(controllers);
   });
   // test cases
 });
