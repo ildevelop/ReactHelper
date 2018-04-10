1be34f827b51b531a9a83b82bf4c598a829e307f
Ilya Radu <raduilya@gmail.com>
2018-04-10 16:27:42 +0300

    Add Login by Gmail

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -74,6 +74,22 @@ class Login extends Component {
       });
 
   }
+  onSignIn(googleUser) {
+    console.log('onSignIn',googleUser);
+    let auth2 = gapi.auth2.getAuthInstance();
+    auth2.signIn().then(function() {
+      if (auth2.isSignedIn.get()) {
+        let profile = auth2.currentUser.get().getBasicProfile();
+        console.log('ID: ' + profile.getId());
+        console.log('Full Name: ' + profile.getName());
+        console.log('Given Name: ' + profile.getGivenName());
+        console.log('Family Name: ' + profile.getFamilyName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail());
+      }
+    });
+
+  }
 
   render() {
     const {redirectToReferrer} = this.state;
@@ -102,6 +118,7 @@ class Login extends Component {
               type="submit"
             />
           </form>
+          <div className="g-signin2" onClick={this.onSignIn.bind(this)} ></div>
         </div>
       )
     }
